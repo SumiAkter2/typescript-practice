9d4b4dcaf3342b943590f69f5363ff7406f0ecf1
SumiAkter2 <akteralisha36@gmail.com>
2023-05-15 11:53:40 +0600

    added spreed and rest

--- src/conceptual-practice/spreed-rest.ts
+++ src/conceptual-practice/spreed-rest.ts
@@ -1,4 +1,17 @@
-const myName: string = 'sumi akter';
+
+// spreed:
+const myName: string[] = ['sumi akter', 'rashed ', 'abdullah'];
 const description:(number| boolean|string)[] = [23, true];
-description.push('[...description,myName]');
-console.log(description);
+description.push(...myName);
+// console.log(description);
+// rest:
+const heroFunc = (...roll: number[]): number => {
+    let sum: number = 0;
+    roll.map(n => {
+        sum = sum + n;
+    })
+    
+    return sum;
+}
+
+console.log(heroFunc(2, 3,6,8,9,235));
